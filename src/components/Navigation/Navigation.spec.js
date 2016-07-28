053d16f805ce5c33676f595468db3a744551236e
PeppeS <pspfolio@gmail.com>
2016-07-28 20:26:19 +0300

    Adding tests for navigation component

--- src/components/Navigation/Navigation.spec.js
+++ src/components/Navigation/Navigation.spec.js
@@ -5,14 +5,22 @@ import { expect } from 'chai';
 
 describe('<Navigation />', () => {
 
+    const navData = [
+        {imgSrc:"http://vignette3.wikia.nocookie.net/rr3/images/e/e8/Circuit_Catalunya_2007.svg/revision/latest?cb=20160413121045",
+        name: "Spanish"},
+        {imgSrc: "http://vignette3.wikia.nocookie.net/rr3/images/e/e8/Circuit_Catalunya_2007.svg/revision/latest?cb=20160413121045",
+        name: "Italian"}
+    ];
+
     it('renders two navigation elements', () => {
-        const data = [
-            {imgSrc:"http://vignette3.wikia.nocookie.net/rr3/images/e/e8/Circuit_Catalunya_2007.svg/revision/latest?cb=20160413121045",
-            name: "Spanish"},
-            {imgSrc: "http://vignette3.wikia.nocookie.net/rr3/images/e/e8/Circuit_Catalunya_2007.svg/revision/latest?cb=20160413121045",
-            name: "Italian"}
-        ];
-        const wrapper = shallow(<Navigation nav={data}/>);
+        const wrapper = shallow(<Navigation nav={navData}/>);
         expect(wrapper.find('.nav')).to.have.length(2);
+    }),
+
+    it('renders image inside navigation element', () => {
+        const wrapper = shallow(<Navigation nav={navData} />);
+        const nav = wrapper.find('.nav').first();
+        expect(nav.find('img')).to.have.length(1);
     })
+
 });
