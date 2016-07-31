0a0f348b9847afe8061812eb7d7468e399c712ca
PeppeS <pspfolio@gmail.com>
2016-07-31 17:18:57 +0300

    ignore-styles for mocha and fixing test after className changed

--- src/components/Navigation/Navigation.spec.js
+++ src/components/Navigation/Navigation.spec.js
@@ -14,19 +14,19 @@ describe('<Navigation />', () => {
 
     it('renders two navigation elements', () => {
         const wrapper = shallow(<Navigation nav={navData}/>);
-        expect(wrapper.find('.nav')).to.have.length(2);
+        expect(wrapper.find('.nav-item')).to.have.length(2);
     }),
 
     it('renders time inside navigation element', () => {
         const wrapper = shallow(<Navigation nav={navData} />);
-        const nav = wrapper.find('.nav').first();
+        const nav = wrapper.find('.nav-item').first();
         expect(nav.find('time')).to.have.length(1);
     }),
 
     it('renders racetrack name inside navigation elment', () => {
         const wrapper = shallow(<Navigation nav={navData} />);
-        const firstNavElement = wrapper.find('.nav').first();
-        const lastNavElement = wrapper.find('.nav').last();
+        const firstNavElement = wrapper.find('.nav-item').first();
+        const lastNavElement = wrapper.find('.nav-item').last();
         expect(firstNavElement.find('p').text()).to.eql('Spanish');
         expect(lastNavElement.find('p').text()).to.eql('Italian');
     })
