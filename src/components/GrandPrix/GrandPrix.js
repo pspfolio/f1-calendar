1a3aa36ff5a18e59b0c948de6fe2ed84df25949b
pspfolio <pspfolio@gmail.com>
2016-09-20 18:55:35 +0300

    resulttable to grandprix component

--- src/components/GrandPrix/GrandPrix.js
+++ src/components/GrandPrix/GrandPrix.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Race from '../Race/Race'
+import ResultTable from '../ResultTable/ResultTable'
 
 import './grandPrix.css'
 
@@ -10,10 +10,32 @@ const Grandprix = React.createClass({
       <div className="grandprix">
         <div className="result">
           <h2>Race result:</h2>
-          {grandprix ? <Race results={grandprix.results} /> : <h2>Not Found</h2>}
+          {grandprix ?
+            <ResultTable
+              headers={[
+                {name: 'Pos.', path: 'position'},
+                {name: 'Name', path: 'Driver.familyName'},
+                {name: 'Constructor', path: 'Constructor.name'},
+                {name: 'Points', path: 'points'},
+                {name: 'Status', path: 'status'}
+              ]}
+              results={ grandprix.results } />
+            : <h2>Not Found</h2> }
         </div>
         <div className="result">
           <h2>Qualifying result:</h2>
+          {grandprix && grandprix.qualifying ?
+            <ResultTable
+              headers={[
+                {name: 'Pos.', path: 'position'},
+                {name: 'Name', path: 'Driver.familyName'},
+                {name: 'Constructor', path: 'Constructor.name'},
+                {name: 'Q1', path: 'Q1'},
+                {name: 'Q2', path: 'Q2'},
+                {name: 'Q3', path: 'Q3'}
+              ]}
+              results={ grandprix.qualifying.results } />
+            : <h2>Not Found</h2> }
         </div>
         <div className="winner">
           <h2>Race winner:</h2>
